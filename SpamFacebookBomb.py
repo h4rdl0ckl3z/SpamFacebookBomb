9eb8da4351d5c38d4058eacf451ed1df6418489f
h65rdl0ckl3z <54482333+h65rdl0ckl3z@users.noreply.github.com>
2020-01-28 23:06:41 +0700

    Update SpamFacebookBomb.py

--- SpamFacebookBomb.py
+++ SpamFacebookBomb.py
@@ -1,8 +1,9 @@
 import fbchat
 from fbchat import Client
 from getpass import getpass
-username = input("Username: ")
-#username = "EAMIL"
+import time
+#username = input("Username: ")
+username = "taypzaangel_427@hotmail.com"
 Client = fbchat.Client(username, getpass())
 no_of_friends = int(input("Number of friends: "))
 for i in range(no_of_friends):
@@ -11,7 +12,10 @@ for i in range(no_of_friends):
     friend = friends[0]
     msg = str(input("Message: "))
     loop = int(input("Loops: "))
+    times = int(input("Delays: "))
     for loop in range(1,loop+1):
+        time.sleep(times)
         sent = Client.send(fbchat.models.Message(msg), friend.uid)
+        print("Loops:", loop, "Message:", msg)
     if sent:
         print("Message sent successfully!")
